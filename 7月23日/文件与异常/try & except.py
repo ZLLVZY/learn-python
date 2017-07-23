2868ad224013d33289cfaceb4817cd54b0b13b7e
X <P752101119@163.COM>
2017-07-23 23:20:36 +0800

    supply IOERROR

--- 7月23日/文件与异常/try & except.py
+++ 7月23日/文件与异常/try & except.py
@@ -1,23 +1,28 @@
-data=open('sketch.txt')
-'''for each_line in data:
+try:
+    data=open('sketch.txt')
+    '''for each_line in data:
     (role,line_spoken)=each_line.split(':',1)
     print(role,end='')
     print(' said:',end='')
     print(line_spoken,end='')
-    无处理'''
-'''for each_line in data:
+    无处理
+    for each_line in data:
     if not each_line.find(':')==-1:
         (role,line_spoken)=each_line.split(':',1)
         print(role,end='')
         print(' said:',end='')
         print(line_spoken,end='')
         针对所有数据的格式写出逻辑'''
-for each_line in data:
-    try:
-        (role,line_spoken)=each_line.split(':',1)
-        print(role,end='')
-        print(' said:',end='')
-        print(line_spoken,end='')
-    except:
-        pass
-data.close()
+        #print(data.readline(),end='')
+        #data.seek(0)
+    for each_line in data:
+        try:
+            (role,line_spoken)=each_line.split(':',1)
+            print(role,end='')
+            print(' said:',end='')
+            print(line_spoken,end='')
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print('The data file is missing!')
