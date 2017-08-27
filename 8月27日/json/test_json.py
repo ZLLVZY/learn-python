778f4c8903841639a9ea406752a677fc709dfee3
X <P752101119@163.COM>
2017-08-28 00:22:28 +0800

    add 8.27 py files

---
+++ 8月27日/json/test_json.py
@@ -0,0 +1,7 @@
+import json
+names =['John',['Johnny','Jack'],'Michael',['Mike','Mikey','Mick']]
+print(names)
+to_transfer=json.dumps(names)
+print(to_transfer)
+from_transfer=json.loads(to_transfer)
+print(names)
